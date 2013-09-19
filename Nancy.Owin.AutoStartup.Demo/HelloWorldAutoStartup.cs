644f975f14c2a9a19275b88d3e41bc8f3f364f53
Steven Robbins <ste.robbins@gmail.com>
2013-09-19 20:43:55 +0100

    Initial Commit

---
+++ Nancy.Owin.AutoStartup.Demo/HelloWorldAutoStartup.cs
@@ -0,0 +1,39 @@
+﻿namespace Nancy.Owin.AutoStartup.Demo
+{
+    using System.Collections.Generic;
+
+    using global::Owin;
+    using global::Owin.AutoStartup;
+
+    // Uncomment this class to see the behaviour when AutoStartups clash
+
+    //public class HelloWorldAutoStartup : IAutoStartup
+    //{
+    //    public string Name
+    //    {
+    //        get
+    //        {
+    //            return "HelloWorld";
+    //        }
+    //    }
+
+    //    public string Path
+    //    {
+    //        get
+    //        {
+    //            return "/";
+    //        }
+    //    }
+
+    //    public IDictionary<string, object[]> DefaultBuilderCommands { get; private set; }
+
+    //    public void Configuration(IAppBuilder builder)
+    //    {
+    //        builder.UseHandlerAsync((req, res) =>
+    //        {
+    //            res.ContentType = "text/plain";
+    //            return res.WriteAsync("Hello, World!");
+    //        });
+    //    }
+    //}
+}
---
+++ Nancy.Owin.AutoStartup.Demo/TestModule.cs
@@ -0,0 +1,10 @@
+﻿namespace Nancy.Owin.AutoStartup.Demo
+{
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["/"] = _ => "Hello world!";
+        }
+    }
+}
